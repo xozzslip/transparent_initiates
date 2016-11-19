4a95e0cc8513f212fd37a955bbc4e1a019d33d7c
Khasan Khafizov <xozzslip@gmail.com>
2016-11-19 17:39:39 +0300

    things

--- initiates/serializers.py
+++ initiates/serializers.py
@@ -6,28 +6,41 @@ from initiates.models import *
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'id')
+        depeth = 1
+        exclude = ('password',)
 
 
 class InitiativeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Initiative
-        fields = ('id', 'url', 'description', 'name', 'creator', 'term', 'customer', 'city', 'pic')
+        fields = ('id', 'url', 'description', 'name', 'creator', 'term', 'customer', 'city', 'pic',
+                  'departments', 'progress')
+        depth = 1
 
 
 class DepartamentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Departament
         fields = ('name', 'agents', 'id')
+        depth = 10  
 
 
 class VerdictSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Verdict
         fields = ('text', 'mark', 'initiative', 'department', 'id')
+        depth = 10
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Comment
         fields = ('text', 'creator', 'initiative', 'id')
+        depth = 10
+
+
+class DepartamentGroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DepartamentGroup
+        fields = ('name')
+        depth = 10
