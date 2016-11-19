ff35f7f86b15c89b0d5f52461ff3287be43bb78a
Khasan Khafizov <xozzslip@gmail.com>
2016-11-19 03:04:17 +0300

    first viewset

--- initiates/views.py
+++ initiates/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework import viewsets
+from django.contrib.auth.models import User, Group
+from initiates.serializers import UserSerializer
 
-# Create your views here.
+from initiates.models import Initiate
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
